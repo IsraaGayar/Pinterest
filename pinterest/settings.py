5136e10a243b097ab6cc4ca070cd2f6a6e4a75b5
IsraaGayar <israa.elgayar@gmail.com>
2021-11-29 19:53:58 +0200

    yarab yb2a sa7 b2a

--- pinterest/settings.py
+++ pinterest/settings.py
@@ -89,8 +89,10 @@ WSGI_APPLICATION = 'pinterest.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'pinterest',
+        'USER': 'iti',
+        'PASSWORD': '123',
     }
 }
 
